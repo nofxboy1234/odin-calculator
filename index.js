4a84e0f5913180c4dd57784e023a6aa3c9ef6248
Dylan Palmboom <nofxboy1234@gmail.com>
2022-06-29 21:38:24 +0200

    Add basic math functions and test in console

---
+++ index.js
@@ -0,0 +1,19 @@
+const add = (num1, num2) => {
+  console.log('add!');
+  return num1 + num2;
+};
+
+const subtract = (num1, num2) => {
+  console.log('subtract!');
+  return num1 - num2;
+};
+
+const multiply = (num1, num2) => {
+  console.log('multiply!');
+  return num1 * num2;
+};
+
+const divide = (num1, num2) => {
+  console.log('divide!');
+  return num1 / num2;
+};
