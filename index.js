26f35b77478bba2a983b2e2a3d9263c9bf759966
Dylan Palmboom <nofxboy1234@gmail.com>
2022-06-29 22:59:13 +0200

    Show number in display for each number click

--- index.js
+++ index.js
@@ -23,15 +23,29 @@ const operate = (operator, num1, num2) => {
 };
 
 const displayNumber = (num) => {
+  console.log(`number to display: ${num}`);
   document.getElementById('screen').textContent = num;
 };
 
-const btn0Click = () => {
-  console.log('button 0 clicked');
-};
+// function displayNumber(num) {
+//   console.log(`number to display: ${num}`);
+//   document.getElementById('screen').textContent = num;
+// }
 
-const btn0 = document.getElementById('btn-0');
-btn0.addEventListener('click', btn0Click);
+// const btn0Click = () => {
+//   console.log('button 0 clicked');
+// };
+
+// const btn0 = document.getElementById('btn-0');
+// btn0.addEventListener('click', btn0Click);
+
+const numbers = document.querySelectorAll('.number');
+numbers.forEach((num) =>
+  num.addEventListener('click', (e) => {
+    // console.log(e.target);
+    displayNumber(e.target.textContent);
+  })
+);
 
 let displayValue = 0;
 displayNumber(displayValue);
