0845533c3e65ce85613bb017acadce5de7738aa6
Dylan Palmboom <nofxboy1234@gmail.com>
2022-06-29 23:26:54 +0200

    Remove commented out code

--- index.js
+++ index.js
@@ -23,29 +23,13 @@ const operate = (operator, num1, num2) => {
 };
 
 const displayNumber = (num) => {
-  // console.log(`number to display: ${num}`);
   displayValue = num;
   document.getElementById('screen').textContent = num;
 };
 
-// function displayNumber(num) {
-//   console.log(`number to display: ${num}`);
-//   document.getElementById('screen').textContent = num;
-// }
-
-// const btn0Click = () => {
-//   console.log('button 0 clicked');
-// };
-
-// const btn0 = document.getElementById('btn-0');
-// btn0.addEventListener('click', btn0Click);
-
 const numbers = document.querySelectorAll('.btn-num');
 numbers.forEach((num) =>
-  num.addEventListener('click', (e) => {
-    // console.log(e.target);
-    displayNumber(e.target.textContent);
-  })
+  num.addEventListener('click', (e) => displayNumber(e.target.textContent))
 );
 
 let displayValue = 0;
