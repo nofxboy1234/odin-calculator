fe4c6d6bfc7d041c83e993c555631074fcb85231
Dylan Palmboom <nofxboy1234@gmail.com>
2022-06-30 20:45:47 +0200

    Replace or append to displayed number

--- index.js
+++ index.js
@@ -24,73 +24,43 @@ const operate = (operator, num1, num2) => {
 };
 
 const storeValue = (value) => {
-  storedValues.push(value);
+  storedValues.push(Number(value));
 };
 
 const displayNumber = (num) => {
   const screen = document.getElementById('screen');
-  if (operatorEntered) {
-    console.log('initializing screen');
-    screen.textContent = num;
-  } else {
-    console.log('appending to screen');
-    screen.textContent = `${screen.textContent}${num}`;
+
+  let lastStoredValue = storedValues.at(-1);
+  if (typeof lastStoredValue === 'number') {
+    if (lastStoredValue === 0) {
+      screen.textContent = num;
+    } else {
+      screen.textContent = screen.textContent.concat(num);
+    }
   }
 };
 
-const calculateSolution = () => {
-  console.log(`operator: ${lastOperator}`);
-  const num1 = storedValues[0];
-  const num2 = storedValues[1];
+// const displayNumberAndOperator = (num, operator) => {
+//   console.log('displayNumberAndOperator');
+// };
 
-  const solution = operate(lastOperator, num1, num2);
-  console.log(`solution: ${solution}`);
-  return solution;
-};
-
-const initializeStoredValues = (num1, operator) => {
-  storedValues = [num1];
-  // lastOperator = operator === '=' ? undefined : operator;
-  lastOperator = operator;
-  canEnterOperator = true;
-  operatorEntered = true;
-};
-
-const numbers = document.querySelectorAll('.btn-num');
-numbers.forEach((num) => {
+const numberButtons = document.querySelectorAll('.btn-num');
+numberButtons.forEach((num) => {
   num.addEventListener('click', (e) => {
-    canEnterOperator = true;
-
-    if (lastOperator === '=') {
-      initializeStoredValues(e.target.textContent, lastOperator);
-      storeValue(e.target.textContent);
-      displayNumber(e.target.textContent);
-    }
-
-    storeValue(e.target.textContent);
     displayNumber(e.target.textContent);
-    operatorEntered = false;
+    storeValue(e.target.textContent);
   });
 });
 
-const operators = document.querySelectorAll('.btn-operator');
-operators.forEach((operator) => {
-  operator.addEventListener('click', (e) => {
-    if (!canEnterOperator) {
-      return;
-    }
+// const operators = document.querySelectorAll('.btn-operator');
+// operators.forEach((operator) => {
+//   operator.addEventListener('click', (e) => {
+//     const solution = calculateSolution();
+//     storeValue(e.target.textContent);
+//     displayNumber(e.target.textContent);
+//   });
+// });
 
-    operatorEntered = true;
-    canEnterOperator = false;
-    const solution = calculateSolution();
-    lastOperator = e.target.textContent;
-    initializeStoredValues(solution, lastOperator);
-    displayNumber(solution);
-  });
-});
-
-let storedValues = [0];
-let lastOperator = '+';
-let canEnterOperator = false;
-let operatorEntered = true;
+let storedValues = [];
+storeValue(0);
 const operatorFunctions = { '+': add, '-': subtract, x: multiply, '/': divide };
